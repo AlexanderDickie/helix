50be73315c43cb1639c645a52867c96ad1e0e575
Philipp Mildenberger <philipp@mildenberger.me>
2023-03-20 00:02:41 +0100

    Remove offset_encoding in CompletionItem

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -4207,7 +4207,6 @@ pub fn completion(cx: &mut Context) {
                 .map(|lsp_item| CompletionItem {
                     language_server_id,
                     lsp_item,
-                    offset_encoding,
                 })
                 .collect();
 
